23f59a76dc57dac3f0e58af995b1f06fd44c01f1
Bett <benzerbett@gmail.com>
2024-08-07 10:52:14 +0300

    Docs with ```sh mkdocs.sh``` script

---
+++ mkdocs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# set -e
+
+# Check that the output directory exists
+if [ ! -d "output" ]; then
+  echo "Error: output directory does not exist. Run the build script first."
+  exit 1
+fi
+
+echo "Writing to docs directory..."
+
+# Make the docs directory if it doesn't exist
+mkdir -p docs
+
+# Copy all html, css, and js files from output to docs
+cp -r output/*.html docs
+cp -r output/*.css docs
+cp -r output/*.js docs
+
+cp -r output/*.json docs
+cp -r output/*.png docs
+cp -r output/*.gif docs
+
+# Copy all files from output/assets to docs/assets
+cp -r output/assets docs
+
+echo "Done!"
